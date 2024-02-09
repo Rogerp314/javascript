ab3505540ce2ade60cd7abccfc6ecd3c328b6177
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-08 23:39:06 -0300

    Outro teste seguido

---
+++ meus testes sozinho/teste36/testes.jsx
@@ -0,0 +1,28 @@
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
+
+
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+
+
+  
