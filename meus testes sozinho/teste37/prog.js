2b944bcfe18bc8b508233911b05936d836fdaf56
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-19 19:02:53 -0300

    teste

---
+++ meus testes sozinho/teste37/prog.js
@@ -0,0 +1,9 @@
+let conteudo = document.querySelector('#teste')
+function clique(){
+    conteudo.innerHTML='Clicou!'
+    conteudo.style.backgroundColor='red'
+}
+function sair(){
+    conteudo.innerHTML='CLique!'
+    conteudo.style.backgroundColor='#0055ff'
+}
